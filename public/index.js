7e12a6904524695b328138ad781344144c644b80
Lior Yitzhak <liorpk01@gmail.com>
2020-05-14 23:50:20 +0300

    Full Responsiveness V1.0

--- public/index.js
+++ public/index.js
@@ -1,12 +1,16 @@
 const root                  = document.querySelector('html');
 const body                  = document.body;
 
+const mainHeaderNav         = body.querySelector('.header-main > nav');
+const mainHeaderHamburger   = body.querySelector('.hamburger-icon');
+const mainHeaderCloseBtn    = body.querySelector('.close-header-main');
+
 const darkModeBtn           = body.querySelector('#dark-mode-icon');
 
 const contactBtns           = Array.from(body.querySelectorAll('.btn-contact'));
 const modalScreens          = Array.from(body.querySelectorAll('.contact-modal-screen'));
 const contactModalScreen    = body.querySelector('.contact-modal-screen'); 
-const closeModalBtn         = body.querySelector('.close-modal'); 
+const closeModalBtn         = body.querySelector('.close-modal');
 
 function addClass(arr, className) {
     arr.forEach(element => element.classList.add(className));
@@ -15,6 +19,14 @@ function addClass(arr, className) {
 function classToggle(arr, className) {
     arr.forEach(element => element.classList.toggle(className));
 }
+
+mainHeaderHamburger.addEventListener('click', () => {
+    mainHeaderNav.style.transform = 'translateX(0)';
+});
+
+mainHeaderCloseBtn.addEventListener('click', () => {
+    mainHeaderNav.style.transform = 'translateX(100%)';
+});
 
 darkModeBtn.addEventListener('click', () => {
     classToggle([body], 'dark-mode');
